521147fdb5ae41bbcd3e2c0b983c9e63050577c2
shahbaz4783 <shahbaz4783@gmail.com>
2023-11-20 01:54:16 +0530

    store data in a json file

--- models/Items.js
+++ models/Items.js
@@ -1,4 +1,5 @@
-const items = [];
+import fs from 'fs';
+import path from 'path';
 
 export default class Item {
 	constructor(title, tag, price, description) {
@@ -9,10 +10,18 @@ export default class Item {
 	}
 
 	save() {
-		items.push(this);
+		const pathOfData = path.join('data', 'products.json');
+		fs.readFile(pathOfData, (err, content) => {
+			let items = [];
+			if (!err) {
+				items = JSON.parse(content);
+			}
+			items.push(this);
+			fs.writeFile(pathOfData, JSON.stringify(items), (err) => {
+				console.log(err);
+			});
+		});
 	}
 
-	static fetchAll() {
-		return items;
-	}
+	static fetchAll() {}
 }
